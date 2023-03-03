9b995e002130415228ad1e8ec057c6c9123b3254
Tom Broucke <tom@tombroucke.be>
2023-03-03 11:04:34 +0100

    chore(bump): Bump version to 3.0.3

--- otomaties-woocommerce-pdf-gift-card.php
+++ otomaties-woocommerce-pdf-gift-card.php
@@ -6,7 +6,7 @@
  * Author URI:      https://tombroucke.be
  * Text Domain:     otomaties-wc-giftcard
  * Domain Path:     /languages
- * Version:           3.0.2
+ * Version:           3.0.3
  *
  * @package         Core
  */
